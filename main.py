5303a559fa85281ba0c3d6831d21080aff711b0d
pinheaded <113788361+pinheaded@users.noreply.github.com>
2024-02-28 16:07:00 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,162 +1,50 @@
-import cv2
-import math
+import cv2 as cv
 import numpy as np
 
-from networktables import NetworkTablesController
+from ultralytics import YOLO
+from ultralytics.engine.results import Results
 
+# Load a model
+model = YOLO('pretrained.pt')  # pretrained YOLOv8n model
 
-class ObjectDetector:
-    def __init__(self, focal_length_x, object_real_width, cascade_model_path) -> None:
-        self.focal_length_x = focal_length_x
-        self.object_real_width = object_real_width
-        self.model = cv2.CascadeClassifier(cascade_model_path)
-        self.max_idx = 0
+cap = cv.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    def calculate_distance_with_offset(self, object_apparent_width: float) -> float:
-        return (lambda distance: distance / 25.4)((self.object_real_width * self.focal_length_x) / object_apparent_width)
+    results: list[Results] = model.predict(frame)
 
-    def calculate_horizontal_angle(self, frame_width: float, object_center_x: float) -> float:
-        screen_center_x = frame_width / 2
-        angle = math.atan((object_center_x - screen_center_x) / self.focal_length_x)
-        return math.degrees(angle) # Angle between horizontal and camera
+    for result in results:
+        as_np: np.ndarray[np.ndarray[np.float32]] = result.boxes.xywh.numpy()
+        try:
+            bool(as_np)
+        except ValueError:
+            as_np = as_np[0].tolist()
+        else:
+            continue
 
-    def detect_objects(self, frame: np.ndarray) -> list[tuple[int,int,int,int]]:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        return self.model.detectMultiScale(gray, 1.3, 5)
+        print("THE ARRAY --", as_np)
+        x, y, w, h = tuple(map(round, as_np))
 
-    
-class ScreenItems:
-    def __init__(self) -> None:
-        pass
+        print("TYPE OF X --", type(x))
 
-    def text_above(self, frame: np.ndarray, text: str, color: tuple, pos: int, bbox: tuple, thickness: int = 1, scale: int = 1) -> None:
-        x, y, w, h = bbox
-        cv2.putText(    
-            frame,
-            text=text,
-            org=(x, y - (pos * 17)), 
-            fontFace=cv2.FONT_HERSHEY_PLAIN,
-            fontScale= scale,
-            color= color, 
-            lineType=cv2.LINE_AA,
-            thickness=thickness
-        )
+        cv.rectangle(frame, (x, y), (x+w, h+y), (255, 0, 0), thickness=2)
 
-    def text_left_bottom(self, frame: np.ndarray, text: str, color: tuple,) -> None:
-        height, width, _ = frame.shape
-        cv2.putText(    
-            frame,
-            text=text,
-            org=(10, height - 20), 
-            fontFace=cv2.FONT_HERSHEY_PLAIN,
-            fontScale= 2,
-            thickness= 1,
-            color= color, 
-            lineType=cv2.LINE_AA
-        )
-    def text_right_bottom(self, frame: np.ndarray, text: str, color: tuple,) -> None:
-        height, width, _ = frame.shape
-        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, 1, 1)
-        text_width, text_height = text_size
+    cv.imshow("object detection", frame)
 
-        cv2.putText(    
-            frame,
-            text=text,
-            org=(width - text_width - 120, height - 20), 
-            fontFace=cv2.FONT_HERSHEY_PLAIN,
-            fontScale= 2,
-            thickness= 1,
-            color= color, 
-            lineType=cv2.LINE_AA
-        )
-    def text_left_up(self, frame: np.ndarray, text: str, color: tuple,) -> None:
-        height, width, _ = frame.shape
-        cv2.putText(    
-            frame,
-            text=text,
-            org=(5, 30), 
-            fontFace=cv2.FONT_HERSHEY_PLAIN,
-            fontScale= 2,
-            thickness= 1,
-            color= color, 
-            lineType=cv2.LINE_AA
-        )
-    def text_right_up(self, frame: np.ndarray, text: str, color: tuple,) -> None:
-        height, width, _ = frame.shape
+    if cv.waitKey(1) == ord("d"):
+        break
 
-        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, 1, 1)
-        text_width, text_height = text_size
+cap.release()
+cv.destroyAllWindows()
 
-        cv2.putText(    
-            frame,
-            text=text,
-            org=(width - text_width - 120, 30), 
-            fontFace=cv2.FONT_HERSHEY_PLAIN,
-            fontScale= 2,
-            thickness= 1,
-            color= color, 
-            lineType=cv2.LINE_AA
-        )
-
-
-
-
-    
-
-def main():
-    focal_length_x = 50 # in mm
-    object_real_width =  (lambda distance_in_inches: distance_in_inches * 25.4)(14.875) #in inches, does conversion to mm
-    cascade_model_path = "path_vision.xml"
-
-    object_detector = ObjectDetector(focal_length_x, object_real_width, cascade_model_path)
-    screen_items = ScreenItems()
-    network_tables_controller = NetworkTablesController()  
-
-
-    cap = cv2.VideoCapture(0)
-    
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        
-        object_counter = 0
-        
-        targets = object_detector.detect_objects(frame)
-        
-
-        for (x, y, w, h) in targets:
-
-            object_counter += 1
-
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), thickness=5)
-            distance = object_detector.calculate_distance_with_offset(w)
-            horizontal_angle = object_detector.calculate_horizontal_angle(frame.shape[1], x + w / 2)
-
-            if -15 < horizontal_angle < 15:
-                angle_description = "Forward"
-            elif horizontal_angle >= 15:
-                angle_description = "Right"
-            else:
-                angle_description = "Left"
-            
-            print(angle_description) # Just for us to see which way the robot needs to turn (for us its the camera moving on its horizontal pivot axis)
-
-            screen_items.text_above(frame,f"Horizontal Angle: {horizontal_angle:.2f} degrees", (255,255,0), 2, (x,y,w,h), 2)
-            screen_items.text_above(frame,f"Object {object_counter}: Distance: {distance:.2f} inches", (255,255,0), 1, (x,y,w,h), 2 )
-            screen_items.text_right_up(frame,f"Move {angle_description}", (255,255,0))
-
-            network_tables_controller.send_data(horizontal_angle, distance)
-
-
-
-        cv2.imshow("Vision Processing", frame)
-        key = cv2.waitKey(1)
-        if key == ord("q") or key == ord("d"):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
+"""
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    result.show()  # display to screen
+    result.save(filename='result.jpg')  # save to disk
+"""
