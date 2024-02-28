c0ac22360cacf58ae761ba385362a9d55c884e7c
Elliott <147102252+ElliottStarosta@users.noreply.github.com>
2024-02-27 19:06:37 -0500

    Configured networking data transmission with distance and angle
    Made OOP

--- networktables.py
+++ networktables.py
@@ -1,26 +1,26 @@
 import ntcore
 import time
-
 # basically copied from https://docs.wpilib.org/en/stable/docs/software/networktables/networktables-intro.html
 # this has been successfully tested between local process <-> local process
 # as well as local process <-> RoboRIO
 # it is yet to be tested between the Orange Pi <-> RoboRIO
 
+
+class NetworkTablesController:
+    def __init__(self) -> None:
+        self.inst = ntcore.NetworkTableInstance.getDefault()
+        self.table = self.inst.getTable("datatable")
+        self.distance_pub = self.table.getDoubleTopic("distance").publish()
+        self.angle_pub = self.table.getDoubleTopic("angle").publish()
+        self.inst.startClient4("example client")
+        self.inst.setServer("localhost")
+        self.inst.setServerTeam(7476, 0)
+        self.inst.startDSClient()
+
+    def send_data(self, angle, distance):
+        self.angle_pub.set(angle)
+        self.distance_pub.set(distance)
+        print(f"Angle: {angle}, Distance: {distance}")
+
 if __name__ == "__main__":
-    inst = ntcore.NetworkTableInstance.getDefault()
-    table = inst.getTable("datatable")
-    xSub = table.getDoubleTopic("x").publish()
-    ySub = table.getDoubleTopic("y").publish()
-    inst.startClient4("example client")
-    inst.setServer("localhost")
-    inst.setServerTeam(7476, 0)
-    inst.startDSClient() # recommended if running on DS computer; this gets the robot IP from the DS
-
-    counter = 0.0
-    while True:
-        time.sleep(0.5)
-
-        x = xSub.set(counter)
-        y = ySub.set(counter)
-        counter += 1
-        print(f"X: {x} Y: {y}")
+    pass
