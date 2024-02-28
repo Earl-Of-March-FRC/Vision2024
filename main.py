ef19c9eceff608edfde58caff328f8b8817075fb
Elliott <147102252+ElliottStarosta@users.noreply.github.com>
2024-02-28 16:43:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import cv2
 import math
 import numpy as np
-
-from networktables import NetworkTablesController
-
+from ultralytics import YOLO
+from ultralytics.engine.results import Results
 
 class ObjectDetector:
-    def __init__(self, focal_length_x, object_real_width, cascade_model_path) -> None:
+    def __init__(self, model_path, focal_length_x, object_real_width, confidence_threshold=0.5) -> None:
         self.focal_length_x = focal_length_x
         self.object_real_width = object_real_width
-        self.model = cv2.CascadeClassifier(cascade_model_path)
-        self.max_idx = 0
+        self.model = YOLO(model_path)
+        self.confidence_threshold = confidence_threshold
 
     def calculate_distance_with_offset(self, object_apparent_width: float) -> float:
         return (lambda distance: distance / 25.4)((self.object_real_width * self.focal_length_x) / object_apparent_width)
@@ -21,10 +20,15 @@ class ObjectDetector:
         return math.degrees(angle) # Angle between horizontal and camera
 
     def detect_objects(self, frame: np.ndarray) -> list[tuple[int,int,int,int]]:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        return self.model.detectMultiScale(gray, 1.3, 5)
+        results = self.model.predict(frame)
 
-    
+        objects = []
+        for result in results.xyxy:
+            x_center, y_center, w, h = map(round, result[:4])
+            objects.append((x_center, y_center, w, h))
+
+        return objects
+
 class ScreenItems:
     def __init__(self) -> None:
         pass
@@ -42,45 +46,6 @@ class ScreenItems:
             thickness=thickness
         )
 
-    def text_left_bottom(self, frame: np.ndarray, text: str, color: tuple,) -> None:
-        height, width, _ = frame.shape
-        cv2.putText(    
-            frame,
-            text=text,
-            org=(10, height - 20), 
-            fontFace=cv2.FONT_HERSHEY_PLAIN,
-            fontScale= 2,
-            thickness= 1,
-            color= color, 
-            lineType=cv2.LINE_AA
-        )
-    def text_right_bottom(self, frame: np.ndarray, text: str, color: tuple,) -> None:
-        height, width, _ = frame.shape
-        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, 1, 1)
-        text_width, text_height = text_size
-
-        cv2.putText(    
-            frame,
-            text=text,
-            org=(width - text_width - 120, height - 20), 
-            fontFace=cv2.FONT_HERSHEY_PLAIN,
-            fontScale= 2,
-            thickness= 1,
-            color= color, 
-            lineType=cv2.LINE_AA
-        )
-    def text_left_up(self, frame: np.ndarray, text: str, color: tuple,) -> None:
-        height, width, _ = frame.shape
-        cv2.putText(    
-            frame,
-            text=text,
-            org=(5, 30), 
-            fontFace=cv2.FONT_HERSHEY_PLAIN,
-            fontScale= 2,
-            thickness= 1,
-            color= color, 
-            lineType=cv2.LINE_AA
-        )
     def text_right_up(self, frame: np.ndarray, text: str, color: tuple,) -> None:
         height, width, _ = frame.shape
 
@@ -98,61 +63,56 @@ class ScreenItems:
             lineType=cv2.LINE_AA
         )
 
-
-
-
-    
-
 def main():
     focal_length_x = 50 # in mm
     object_real_width =  (lambda distance_in_inches: distance_in_inches * 25.4)(14.875) #in inches, does conversion to mm
-    cascade_model_path = "path_vision.xml"
+    model_path = 'pretrained.pt'
 
-    object_detector = ObjectDetector(focal_length_x, object_real_width, cascade_model_path)
+    object_detector = ObjectDetector(model_path, focal_length_x, object_real_width)
     screen_items = ScreenItems()
-    network_tables_controller = NetworkTablesController()  
-
 
     cap = cv2.VideoCapture(0)
     
     while True:
         ret, frame = cap.read()
-        if not ret:
-            break
-        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
         object_counter = 0
-        
-        targets = object_detector.detect_objects(frame)
-        
 
-        for (x, y, w, h) in targets:
+        results: list[Results] = object_detector.model.predict(frame)
 
-            object_counter += 1
-
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), thickness=5)
-            distance = object_detector.calculate_distance_with_offset(w)
-            horizontal_angle = object_detector.calculate_horizontal_angle(frame.shape[1], x + w / 2)
-
-            if -15 < horizontal_angle < 15:
-                angle_description = "Forward"
-            elif horizontal_angle >= 15:
-                angle_description = "Right"
+        for result in results:
+            # object_counter += 1
+            as_np: np.ndarray[np.ndarray[np.float32]] = result.boxes.xywh.numpy()
+            try:
+                bool(as_np)
+            except ValueError:
+                as_np = as_np[0].tolist()
             else:
-                angle_description = "Left"
-            
-            print(angle_description) # Just for us to see which way the robot needs to turn (for us its the camera moving on its horizontal pivot axis)
+                continue
 
-            screen_items.text_above(frame,f"Horizontal Angle: {horizontal_angle:.2f} degrees", (255,255,0), 2, (x,y,w,h), 2)
-            screen_items.text_above(frame,f"Object {object_counter}: Distance: {distance:.2f} inches", (255,255,0), 1, (x,y,w,h), 2 )
-            screen_items.text_right_up(frame,f"Move {angle_description}", (255,255,0))
+            print("THE ARRAY --", as_np)
+            x_center, y_center, w, h = tuple(map(round, as_np))
 
-            network_tables_controller.send_data(horizontal_angle, distance)
+            # Calculate top-left corner coordinates based on center
+            x_top_left = int(x_center - w / 2)
+            y_top_left = int(y_center - h / 2)
+
+            cv2.rectangle(frame, (x_top_left, y_top_left), (x_top_left+w, y_top_left+h), (255, 255, 0), thickness=2)
+
+            distance = object_detector.calculate_distance_with_offset(w)
+            horizontal_angle = object_detector.calculate_horizontal_angle(frame.shape[1], x_center + w / 2)
+
+             
+
+            screen_items.text_above(frame,f"Horizontal Angle: {horizontal_angle:.2f} degrees", (255,255,0), 2, (x_top_left,y_top_left,w,h), 2)
+            screen_items.text_above(frame,f"Object {object_counter}: Distance: {distance:.2f} inches", (255,255,0), 1, (x_top_left,y_top_left,w,h), 2 )
+            # screen_items.text_right_up(frame,f"Move {angle_description}", (255,255,0))
 
 
+        cv2.imshow("object detection", frame)
 
-        cv2.imshow("Vision Processing", frame)
-        key = cv2.waitKey(1)
-        if key == ord("q") or key == ord("d"):
+        if cv2.waitKey(1) == ord("d"):
             break
 
     cap.release()
