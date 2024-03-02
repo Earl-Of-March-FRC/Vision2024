b4de6bca45e444d7407fb05038f0c4e6773e3a84
Elliott <147102252+ElliottStarosta@users.noreply.github.com>
2024-03-01 22:36:36 -0500

    [Elliott] Added cropped image size and vid_stride
    Still includes multiprocessing and queueing, the average FPS for my machine is 40 FPS with this code.
    
    Base multiprocessing around 33 FPS
    Base vid_stride and cropped around 30 FPS

--- main.py
+++ main.py
@@ -8,15 +8,23 @@ import numpy as np
 from ultralytics import YOLO
 from ultralytics.engine.results import Results
 
-from typing import TypeVar, TYPE_CHECKING
-
+from typing import TypeVar
 from multiprocessing import Queue
+import time
 
 T = TypeVar("T")
 MatLike = np.ndarray[T]
 
-# THIS IS FOR MY OWN WEBCAM, REMEMBER TO CALIBRATE THE CAMERA AND REPLACE THIS
-cam_matrix = np.array([[658.86677309, 0, 324.01396488], [0, 658.59117981, 234.71600824], [0, 0, 1]])
+
+'''
+This is configured to Elliott's webcam 
+Haoyan's: cam_matrix = np.array([[658.86677309, 0, 324.01396488], [0, 658.59117981, 234.71600824], [0, 0, 1]])
+
+'''
+
+cam_matrix = np.array([[508.13950658192897, 0, 315.83545490013387], 
+                       [0, 508.4437534984872, 244.77465580560457], 
+                       [0, 0, 1]])
 
 class ObjectDetector:
     def __init__(
@@ -33,7 +41,7 @@ class ObjectDetector:
 
     def calculate_distance_with_offset(self, detection_width: float) -> float:
         return (lambda distance: distance / 25.4)((self.object_real_width * self.focal_length_x) / detection_width)
-
+    
     def calculate_horizontal_angle(self, frame: MatLike, object_center_x: float) -> float:
         """
         https://stackoverflow.com/questions/55080775/opencv-calculate-angle-between-camera-and-object my beloved
@@ -53,53 +61,22 @@ class ObjectDetector:
 
         return real_angle
     
-class Worker(ObjectDetector):
-    def __init__(
-            self, 
-            model_path: str, 
-            focal_length_x: float, 
-            object_real_width: float
-        ) -> None:
-        super().__init__(model_path, focal_length_x, object_real_width)
+    def cropped(self, frame: MatLike, /, top_left_x: int, top_left_y: int, new_width: int, new_height: int) -> MatLike:
+        return frame[top_left_y:top_left_y+new_height,top_left_x:top_left_x+new_width]
 
-    def process_frame(self, frame: MatLike) -> MatLike:
-        results: list[Results] = self.model.predict(frame)
-        processed_frame = frame.copy()
-        controller = NetworkTablesController()  
-        for result in results:
-            as_np: np.ndarray[np.ndarray[np.float32]] = result.boxes.xywh.numpy()
-            try:
-                bool(as_np)
-            except ValueError:
-                as_np = as_np[0].tolist()
-            else:
-                continue
+    def detect_objects(self, frame: MatLike, /) -> list[tuple[int,int,int,int]]:
+        results = self.model.predict(frame)
 
-            x_center, y_center, w, h = tuple(map(round, as_np))
+        objects = []
+        for result in results.xyxy:
+            x_center, y_center, w, h = map(round, result[:4])
+            objects.append((x_center, y_center, w, h))
 
-            x_left = int(x_center - w / 2)
-            y_top = int(y_center - h / 2)
-
-            cv2.rectangle(processed_frame, (x_left, y_top), (x_left+w, y_top+h), (255, 255, 0), thickness=2)
-
-            angle = self.calculate_horizontal_angle(frame, x_center)
-            distance_object = self.calculate_distance_with_offset(w)
-
-            controller.send_data(angle,distance_object)
-
-            ScreenItems.text_above(processed_frame,f"Horizontal Angle: {angle:.2f} degrees", (255,255,0), 2, (x_left,y_top,w,h), 2)
-            ScreenItems.text_above(processed_frame,f"Distance: {distance_object:.2f} in", (255,255,0), 1, (x_left,y_top,w,h), 2)
-
-            processed_frame = cv2.circle(processed_frame, (x_center, y_center), 1, (0,255,0), 2)
-
-        return processed_frame
-
-
-
+        return objects
 
 class ScreenItems:
     @staticmethod
-    def text_above(frame: MatLike, text: str, color: tuple, pos: int, bbox: tuple, thickness: int = 1, scale: int = 1) -> None:
+    def text_above(frame: MatLike, /, text: str, color: tuple, pos: int, bbox: tuple, thickness: int = 1, scale: int = 1) -> None:
         x, y, w, h = bbox
         cv2.putText(
             frame,
@@ -113,7 +90,7 @@ class ScreenItems:
         )
 
     @staticmethod
-    def text_right_up(frame: MatLike, text: str, color: tuple) -> None:
+    def text_right_up(frame: MatLike, /, text: str, color: tuple) -> None:
         height, width, _ = frame.shape
 
         text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, 1, 1)
@@ -146,11 +123,52 @@ class NetworkTablesController:
         self.distance_pub.set(distance)
         print(f"Angle: {angle}, Distance: {distance}")
 
-def main():
+class Worker(ObjectDetector):
+    def __init__(
+            self, 
+            model_path: str, 
+            focal_length_x: float, 
+            object_real_width: float
+        ) -> None:
+        super().__init__(model_path, focal_length_x, object_real_width)
 
-    focal_length_x = 658.867 # in mm
-    object_real_width =  (lambda distance_in_inches: distance_in_inches * 25.4)(14.875) #in inches, does conversion to mm
-    model_path = 'pretrained.pt'
+    def process_frame(self, frame: MatLike) -> MatLike:
+        results: list[Results] = self.model.predict(frame, imgsz=(640, 320), vid_stride=5, max_det=1)
+        processed_frame = frame.copy()
+        controller = NetworkTablesController()  
+        for result in results:
+            as_np: np.ndarray[np.ndarray[np.float32]] = result.boxes.xywh.numpy()
+            try:
+                bool(as_np)
+            except ValueError:
+                as_np = as_np[0].tolist()
+            else:
+                continue
+
+            x_center, y_center, w, h = tuple(map(round, as_np))
+
+            x_left = int(x_center - w / 2)
+            y_top = int(y_center - h / 2)
+
+            cv2.rectangle(processed_frame, (x_left, y_top), (x_left+w, y_top+h), (255, 255, 0), thickness=2)
+
+            angle = self.calculate_horizontal_angle(frame, x_center)
+            distance_object = self.calculate_distance_with_offset(w)
+
+            controller.send_data(angle,distance_object)
+
+            ScreenItems.text_above(processed_frame,f"Horizontal Angle: {angle:.2f} degrees", (255,255,0), 2, (x_left,y_top,w,h), 2)
+            ScreenItems.text_above(processed_frame,f"Distance: {distance_object:.2f} in", (255,255,0), 1, (x_left,y_top,w,h), 2)
+
+            processed_frame = cv2.circle(processed_frame, (x_center, y_center), 1, (0,255,0), 2)
+
+        return processed_frame
+
+
+def main():
+    focal_length_x = 508.13950658192897  # in mm
+    object_real_width =  (lambda distance_in_inches: distance_in_inches * 25.4)(14.875)  # in inches, does conversion to mm
+    model_path = 'Vision2024\\pretrained.pt'
 
     input_q = Queue()
     output_q = Queue()
@@ -158,14 +176,29 @@ def main():
     worker = Worker(model_path, focal_length_x, object_real_width)
     cap = cv2.VideoCapture(0)
 
+    fps = 0
+    frame_count = 0
+
     while True:
         ret, frame = cap.read()
         if not ret:
             break
+
+        frame = worker.cropped(frame, top_left_x=0, top_left_y=64, new_width=frame.shape[1], new_height=320)
+
         input_q.put(frame)
         processed_frame = worker.process_frame(input_q.get())
         output_q.put(processed_frame)
         cv2.imshow("object detection", processed_frame)
+
+        start = time.monotonic()
+        results: list[Results] = worker.model.predict(frame, imgsz=(640, 320), vid_stride=5, max_det=1)
+        end = time.monotonic()
+        
+        frame_count += 1
+        fps += 1 / (end - start or 0.00001)
+        print("FRAMES PER SECOND --", fps / frame_count)
+
         if cv2.waitKey(1) == ord("d"):
             break
 
