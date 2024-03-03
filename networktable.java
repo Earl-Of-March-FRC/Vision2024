05019b123a46023d1ab7823533df7f8dd9c0ca32
Elliott <147102252+ElliottStarosta@users.noreply.github.com>
2024-03-02 21:33:59 -0500

    Update networktable.java

--- networktable.java
+++ networktable.java
@@ -1,41 +1,39 @@
 package frc.robot.Subsystems;
 
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
-  private final NetworkTableInstance inst;
+    private final NetworkTableInstance inst;
 
-  private final NetworkTable table;
-  private final NetworkTableEntry distanceEntry;
-  private final NetworkTableEntry angleEntry;
+    private final NetworkTable table;
+    private final NetworkTableEntry distanceEntry;
+    private final NetworkTableEntry angleEntry;
 
-  public VisionSubsystem() {
-      inst = NetworkTableInstance.getDefault();
-      inst.startClient3("example client");
-      inst.setServer("localhost");
-      inst.setServerTeam(9127, 0);
+    public VisionSubsystem() {
+        inst = NetworkTableInstance.getDefault();
+        inst.startServer(); // Start the server on the Java side
+        inst.setServerTeam(9127, 0); // Set the team number
 
-      table = inst.getTable("datatable");
-      distanceEntry = table.getEntry("distance");
-      angleEntry = table.getEntry("angle");
-  }
+        table = inst.getTable("vision"); // Updated table name
+        distanceEntry = table.getEntry("distance");
+        angleEntry = table.getEntry("angle");
+    }
 
-  @Override
-  public void periodic() {
-    System.out.println(getDistance() + ", " + getAngle());
-  }
+    @Override
+    public void periodic() {
+        System.out.println(getDistance() + ", " + getAngle());
+    }
 
-  public double getDistance() {
-      // Get distance from NetworkTable
-      return distanceEntry.getDouble(0.0); // Default value is 0.0 if entry is not found
-  }
+    public double getDistance() {
+        // Get distance from NetworkTable
+        return distanceEntry.getDouble(-1.0); // Default value is -1.0 if entry is not found
+    }
 
-  public double getAngle() {
-      // Get angle from NetworkTable
-      return angleEntry.getDouble(0.0); // Default value is 0.0 if entry is not found
-  }
+    public double getAngle() {
+        // Get angle from NetworkTable
+        return angleEntry.getDouble(-1.0); // Default value is -1.0 if entry is not found
+    }
 }
